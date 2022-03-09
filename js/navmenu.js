54941d8d1816244b1a222880fab9e370c0612731
RDatsko <101229411+RDatsko@users.noreply.github.com>
2022-03-10 06:58:27 +0900

    Create navmenu.js

---
+++ js/navmenu.js
@@ -0,0 +1,48 @@
+document.writeln(`
+<nav>
+<ul>
+    <li>Home</li>
+    <li>Hardware</li>
+    <li>Software</li>
+    <li>
+        HTML
+        <div>
+            <div>
+                <b><u>Web Apps</u></b><br>
+                None<br>
+            </div>                                
+            <div>
+                <b><u>Games</u></b><br>
+                Trivia<br>
+                The Wheel<br>
+                Clash<br>
+            </div>                                
+            <div>
+                <b><u>Office</u></b><br>
+                Writer<br>
+                Sheets<br>
+                Presentation<br>
+            </div>                                
+            <div>
+                <b><u>Tools</u></b><br>
+                HTML Coder<br>
+            </div>                                
+            <div>
+                <b><u>Media</u></b><br>
+                Video Player<br>
+                Interactive Book<br>
+            </div>
+        </div>
+    </li>
+    <li>
+        WebXR
+        <div class="nav-webxr">
+            <div style="grid-column: span 3;">About</div>
+            <div style="grid-column: span 3;">Wiki</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+        </div>
+    </li>
+</ul>
+</nav>`);
