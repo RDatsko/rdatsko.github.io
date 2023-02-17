1bd1d777bd56bea725f2f57145b6235e07ea9b14
RDatsko <101229411+RDatsko@users.noreply.github.com>
2023-02-18 08:18:18 +0900

    Update navmenu.js

--- js/navmenu.js
+++ js/navmenu.js
@@ -33,7 +33,7 @@ document.writeln(`
     <li>
     <label for="nav-xrml">XRML</label>
         <div class="accordion__content nav-xrml">
-            <div><a href="/pages/xrml/"">About</a></div>
+            <div><a href="/pages/xrml/">About</a></div>
             <div"><a href="/pages/xrml/wiki/">Wiki</a></div>
             <div"><a href="/pages/xrml/nehe/">NeHe based tutorials</a></div>
             <div"><a href="/pages/xrml/test.html">Wiki</a></div>
