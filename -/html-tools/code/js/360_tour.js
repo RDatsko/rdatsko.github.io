1a93c479f1f4db957c8cd425e8f51484face5897
RDatsko <101229411+RDatsko@users.noreply.github.com>
2025-07-25 15:50:25 +0900

    .

--- -/html-tools/code/js/360_tour.js
+++ -/html-tools/code/js/360_tour.js
@@ -7,12 +7,12 @@ pannellum.viewer('Summerhill', {
         "showFullscreenCtrl": true
     },
     "scenes": {
-        "k5classroom": {
-            "title": "K5 Classroom",
+        "1Fhallway": {
+            "title": "1F Hallway",
             "type": "equirectangular",
             "compass": true,
             "northOffset": 0,
-            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_3F-K5.jpg",
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_1F-hallway.jpg",
             "autoLoad": true,
             "hotSpots": [
                 {
@@ -24,12 +24,182 @@ pannellum.viewer('Summerhill', {
                 },
             ]
         },
-        "kitchen": {
+        "1Fimaginarium": {
+            "title": "Imaginarium Classroom",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_1F-imaginarium.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "1Fsunset": {
+            "title": "Sunset Classroom",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_1F-sunset.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "1Fsunshine": {
+            "title": "Sunshine Classroom",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_1F-sunshine.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "2Fhallway": {
+            "title": "2F Hallway",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_2F-hallway.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "2Ffullmoon": {
+            "title": "Full Moon Classroom",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_2F-fullmoon.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "2Fhalfmoon": {
+            "title": "Half Moon Classroom",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_2F-halfmoon.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "3Fhallway": {
+            "title": "3F Hallway",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_3F-hallway.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "3Fk5classroom": {
+            "title": "K5 Classroom",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_3F-k5.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "3FKitchen": {
             "title": "Kitchen",
             "type": "equirectangular",
             "compass": true,
             "northOffset": 0,
-            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_3F-Kitchen.jpg",
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_3F-kitchen.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "3Flibrary": {
+            "title": "Library and Gym",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_3F-library-gym.jpg",
+            "autoLoad": true,
+            "hotSpots": [
+                {
+                    "pitch": 0,
+                    "yaw": -150,
+                    "type": "scene",
+                    "text": "Kitchen",
+                    "sceneId": "kitchen"
+                },
+            ]
+        },
+        "playground": {
+            "title": "Outdoor Playground",
+            "type": "equirectangular",
+            "compass": true,
+            "northOffset": 0,
+            "panorama": "https://rdatsko.github.io/aoba/360/images/Summerhill_playground.jpg",
             "autoLoad": true,
             "hotSpots": [
                 {
